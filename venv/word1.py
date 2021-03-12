8bc4ce9d8f59095277bbb68438011ed9773f674e
gchlepos <georgechlepos@gmail.com>
2021-03-13 01:06:22 +0200

    added word encryption

--- venv/word1.py
+++
@@ -1,8 +0,0 @@
-class word:
-    def __init__(self, word, encryptedWord) -> None:
-        self.word = word
-        self.encryptedWord = encryptedWord
-
-    def __str__(self) -> str:
-        return f"Word: {word}"
-
